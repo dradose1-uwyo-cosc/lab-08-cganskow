7b96603ea0adc9faf89df1dcf9319cae2c8fdde5
Caitlyn Ganskow <cganskow@uwyo.edu>
2024-11-07 22:46:59 +0000

    Almost Done

--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -1,18 +1,19 @@
-# Your Name Here
+# Caitlyn Ganskow
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section:
-# Sources, people worked with, help given to:
-# your
-# comments
-# here
+# 11/7/2024
+# Lab 08
+# Lab Section: 12
+# Sources, people worked with, help given to: None
 
 
 # Write a function that will properly check strings to see if they are an int or float, and convert them if so
 # If they can't be converted return false
 # Other wise return the converted int or float 
 # Floats should only have one decimal point in them 
+
+def int_or_float(number):
+    pass
+
 
 
 print("*" * 75)
@@ -37,6 +38,39 @@ print("*" * 75)
 # Exit on the word exit
 # Remember all inputs are strings, but the function needs ints or floats
 # Call your function and print the resulting list
+
+
+
+def point_slope(m, b, lx, ux):
+    """collects 4 numbers then uses point-slope formula to return values of y within range given"""
+    y_values = []
+    if type(lx) == type(1.0) or type(ux) == type(1.0):
+        return False
+    elif int(lx) > int(ux):
+        return "if 2"
+    elif (type(m) == type(1.0) or type(m) == type(1)) and (type(b) == type(1.0) or type(b) == type(1)):
+        for x in range(lx, ux):
+            y = m*x + b
+            y_values.append(y)
+        return y_values
+    else:
+        return "final no"
+
+while True:
+    user_input = input("please input a m, b, lower x, upper x: type 'exit' to quit")
+    if user_input.lower() == 'exit':
+        break
+    else:
+        numbers = user_input.split(", ")
+        print(numbers)
+        um = int(numbers[0])
+        ub = int(numbers[1])
+        ulx = int(numbers[2])
+        uux = int(numbers[3])
+        print(point_slope(um, ub, ulx, uux))
+
+
+
 
 print("*" * 75)
 
